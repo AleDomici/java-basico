e331298b5c8fea611052f92e93a32acc1420abf9
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 14:43:50 -0300

    feat: adicionar cálculo e exibição do fatorial de um número inteiro

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
 
+
 public class Fatorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
-        System.out.println();
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        int fatorial = 1; //não pode multiplicar por 0
+
+        String expressao = "";
+
+        // Loop while para calcular o fatorial
+        for (int index = 1; index <= numero; index++) {
+            fatorial *= index;
+
+            if (index == numero) {
+                expressao += index; //Não coloca  *  no último número
+            } else {
+                expressao += index + " * "; //coloca o * entre os numeros
+            }
+        }
+
+        System.out.println(fatorial + " (" + expressao + ")");
+
+        entrada.close();
 
     }
-
 }
