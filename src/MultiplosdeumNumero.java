89006dc50a849bd4d0a9f9ae966a105f96257a2b
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 16:43:53 -0300

    feat: criar classe para múltiplos de um número e solicitar entrada de n e m

--- src/MultiplosdeumNumero.java
+++ src/MultiplosdeumNumero.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class MultiplosdeumNumero {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o número n: ");
+        int n = entrada.nextInt();
+
+        System.out.print("Digite o número m: ");
+
+        System.out.print("Múltiplos de " + n + " até " + m + ": ");
     }
 }
