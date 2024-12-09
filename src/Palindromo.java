cc704f28e58d28d84a12a8c0ecf2f5440e6b35cb
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 16:21:13 -0300

    feat: adicionar lógica para inverter um número

--- src/Palindromo.java
+++ src/Palindromo.java
@@ -4,5 +4,17 @@ public class Palindromo {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Digite um número: ");
+        int numero = entrada.nextInt();
+
+        int original = numero;
+        int invertido = 0;
+
+        //loop que inverte o numero
+        while (numero != 0) {
+            invertido = invertido * 10 + numero % 10;
+            numero /=10;
+        }
+
     }
 }
