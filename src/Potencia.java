2feeab99b05d0f3716c434c2ff05aa9a4a7866e4
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 16:04:54 -0300

    feat: adiciona entrada de dados para base e expoente

--- src/Potencia.java
+++ src/Potencia.java
@@ -4,5 +4,15 @@ public class Potencia {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
+        System.out.print("Digite a base: ");
+        int base = entrada.nextInt();
+
+        System.out.println("Digite o expoente: ");
+
+        int expoente = entrada.nextInt();
+
+        int resultado = 1;
+
+
     }
 }
