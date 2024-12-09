6f939d4a66cc65d2818b40382da25fbd2ee48c3c
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 15:34:03 -0300

    feat: criando classe NumerosPrimos para verificação de números primos

--- src/NumerosPrimos.java
+++ src/NumerosPrimos.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
 
 public class NumerosPrimos {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite um número inteiro: ");
-        int numero = entrada.nextInt();
+            System.out.print("Digite um número inteiro: ");
+            int numero = entrada.nextInt();
 
-        int numerointervido = 0;
-
-        // Loop para inverter os dígitos do numero
-        while (numero != 0) { // laço vai continuar até que o número se torne zero.
-            int digito = numero %10; //pega o último dígito do número. O operador % é o módulo, que calcula o resto da divisão do número por 10.
-            numerointervido = numerointervido *10 + digito;
-            numero /=10; //Aqui é onde ocorre a inversão
         }
-        System.out.println("Número invertido: " + numerointervido);
-
-        entrada.close();
     }
-}
+
