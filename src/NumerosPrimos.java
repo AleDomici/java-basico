9cbc16bc7014defe068b54a6827354651255a7f1
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 15:16:06 -0300

    Implementa a inversão dos dígitos de um número com um loop while, usando módulo e divisão

--- src/NumerosPrimos.java
+++ src/NumerosPrimos.java
@@ -4,5 +4,17 @@ public class NumerosPrimos {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        int numerointervido = 0;
+
+        // Loop para inverter os dígitos do numero
+        while (numero != 0) { // laço vai continuar até que o número se torne zero.
+            int digito = numero %10; //pega o último dígito do número. O operador % é o módulo, que calcula o resto da divisão do número por 10.
+            numerointervido = numerointervido *10 + digito;
+            numero /=10; //Aqui é onde ocorre a inversão
+        }
+
     }
 }
