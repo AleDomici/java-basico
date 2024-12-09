79201f9b8560623c6c5648db3ca1c2885fe247fc
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-09 15:55:33 -0300

    feat: adicionar função verificarPrimo para checar primalidade

--- src/NumerosPrimos.java
+++ src/NumerosPrimos.java
@@ -14,3 +14,17 @@ public class NumerosPrimos {
             entrada.close();
         }
 
+        //função para verificar se é primo
+        public static boolean verificarPrimo(int numero) {
+            if (numero <= 1) {
+                return false; //verifica se o numero é menor ou igual a 1 pois não sao primos
+            }
+
+            for (int i = 2; i <= Math.sqrt(numero); i++){ // calcula a raiz quadrada do numero
+                if (numero % i ==0) {
+                    return false; // Se for divisível por algum número, não é primo
+                }
+            }
+            return true; //se não for divisivel, é primo
+        }
+    }
